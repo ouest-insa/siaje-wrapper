02c65f9e8b432bf0b4576e1272dd246cf204a245
Quentin Dufour <quentin@dufour.tk>
2014-12-03 00:35:58 +0100

    Can now retrieve studies from SIAJE

--- test/test.siaje.js
+++ test/test.siaje.js
@@ -2,30 +2,38 @@ var should = require('chai').should(),
     conf = require('../config'),
     Siaje = require('../lib/siaje');
 
-describe('#connect', function() {
+describe('new Siaje()', function() {
     
     it('success to connect to SIAJE',function(done) {
-        var s2 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err) {
+        var s = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err) {
            should.not.exist(err); 
            done();
         });
     });
     
     it('fail to connect to SIAJE',function(done) {
-        var s1 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass+'a', function(err) {
+        var s = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass+'a', function(err) {
            err.should.equal('Wrong username or password'); 
            done();
         });
     });
    
-    it('success to connect after a failure. SIAJE security against bruteforcing',function(done) {
+    it('success to connect after a failure. SIAJE security against bruteforcing', function(done) {
         this.timeout(10000);
         setTimeout(function() {
-        var s22 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err2) {
-           should.not.exist(err2); 
+        global.s = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err) {
+           should.not.exist(err); 
            done();
         });
         }, 3000);
     });
+});
 
+describe('Siaje.getStudies()', function() {
+    it('retrieve studies',function(done) {
+        global.s.getStudies(function(err,res) {
+           should.not.exist(err);
+           done();
+        });
+    });
 });
