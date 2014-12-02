318f72789a753299216ee1002407e935e3050c2a
Quentin Dufour <quentin@dufour.tk>
2014-12-02 23:02:39 +0100

    Handle siaje security against bruteforcing

--- test/test.siaje.js
+++ test/test.siaje.js
@@ -3,10 +3,29 @@ var should = require('chai').should(),
     Siaje = require('../lib/siaje');
 
 describe('#connect', function() {
-    it('connect to SIAJE',function(done) {
-        var s = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err) {
+    
+    it('success to connect to SIAJE',function(done) {
+        var s2 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err) {
            should.not.exist(err); 
            done();
         });
     });
+    
+    it('fail to connect to SIAJE',function(done) {
+        var s1 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass+'a', function(err) {
+           err.should.equal('Wrong username or password'); 
+           done();
+        });
+    });
+   
+    it('success to connect after a failure. SIAJE security against bruteforcing',function(done) {
+        this.timeout(10000);
+        setTimeout(function() {
+        var s22 = new Siaje(conf.connection.je, conf.connection.login, conf.connection.pass, function(err2) {
+           should.not.exist(err2); 
+           done();
+        });
+        }, 3000);
+    });
+
 });
